327f21a1aec5fb0ac2a36d922ecba6435225c635
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2024-05-04 00:26:07 +0600

    .

---
+++ FinalLab - 03/product-management-system-api/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import config from 'ormconfig';
+
+@Module({
+  imports: [AuthModule, TypeOrmModule.forRoot(config), UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
